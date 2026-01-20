a1b4c45a1f2e1a810a1567104265517a473bbaf1
omkar-pawar-globalion <omkar.pawar@globalion.in>
2026-01-20 10:40:37 +0530

    Update serializers.py
    refactor(serializers): simplify serialization to ObjectId conversion only

--- utils/serializers.py
+++ utils/serializers.py
@@ -1,25 +1,13 @@
 from bson import ObjectId
 
-def serialize_project(project: dict) -> dict:
-    if not project:
+def serialize_ids_only(doc: dict) -> dict:
+    if not doc:
         return None
 
-    return {
-        "id": str(project["_id"]),
-        "name": project["name"],
-        "client_name": project["client_name"],
-        "description": project.get("description"),
-        "estimation_technique": project["estimation_technique"],
-        "target_margin": project["target_margin"],
-        "risk_buffer": project["risk_buffer"],
-        "negotiation_buffer": project["negotiation_buffer"],
-        "estimated_team_size": project["estimated_team_size"],
-        "status": project["status"],
-        "company_id": str(project["company_id"]),
-        "created_by": str(project["created_by"]),
-        "created_at": project["created_at"],
-    }
+    doc["id"] = str(doc.pop("_id"))
 
+    for key, value in doc.items():
+        if isinstance(value, ObjectId):
+            doc[key] = str(value)
 
-def serialize_project_list(projects: list) -> list:
-    return [serialize_project(p) for p in projects]
+    return doc
